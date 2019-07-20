6fffddd6d0fe74d0f15722ab3916d0c77da9f523
Jixian Wang <hellojixian@gmail.com>
2019-07-20 12:26:14 +0200

    update

--- learn.py
+++ learn.py
@@ -8,74 +8,79 @@ Visit my tutorial website for more: https://morvanzhou.github.io/tutorials/
 import numpy as np
 import matplotlib.pyplot as plt
 
-TARGET_DNA=[6,9,3,1,8,2,3,1,3,4,2,1,2,3,10,0,10,1,2,2,3,1,3,4]
 
-DNA_SIZE = len(TARGET_DNA)  # DNA (real number)
-DNA_BOUND = [0, 10]       # solution upper and lower bounds
+np.set_printoptions(edgeitems=20)
+np.core.arrayprint._line_width = 280
+
 N_GENERATIONS = 2000
-POP_SIZE = 100           # population size
-N_KID = 50               # n kids per generation
+TARGET_DNA = [6,9,3,1,8,2,3,1,3,4,2,1,2,3,10,0,10,1,2,2,3,1,3,4,1,2,2,3]
 
-np.set_printoptions(edgeitems=10)
-np.core.arrayprint._line_width = 180
+class Learner(object):
 
-def F(x): return np.sin(10*x)*x + np.cos(2*x)*x     # to find the maximum of this function
-
-
-# find non-zero fitness for selection
-def get_fitness(pred):     
-    v = (np.round(TARGET_DNA,1) == np.round(pred,1)).sum(axis=1)    
-    # v = np.sqrt(np.mean(TARGET_DNA - pred, axis=1)**2)
-    return v
-
-
-def make_kid(pop, n_kid):
-    # generate empty kid holder
-    kids = {'DNA': np.empty((n_kid, DNA_SIZE))}
-    kids['mut_strength'] = np.empty_like(kids['DNA'])
-
-    for kv, ks in zip(kids['DNA'], kids['mut_strength']):
-        # crossover (roughly half p1 and half p2)        
-        p1, p2 = np.random.choice(np.arange(POP_SIZE), size=2, replace=False)
+    def __init__(self, pop_size, n_kid):
+        self.TARGET_DNA = TARGET_DNA
+        self.DNA_size = len(self.TARGET_DNA)        
+        self.DNA_bound = [0, 10]
+        self.pop_size = pop_size
+        self.n_kid = n_kid
         
-        cp = np.random.randint(0, 2, DNA_SIZE, dtype=np.bool)  # crossover points                
-        kv[cp] = pop['DNA'][p1, cp]
-        kv[~cp] = pop['DNA'][p2, ~cp]
-        ks[cp] = pop['mut_strength'][p1, cp]
-        ks[~cp] = pop['mut_strength'][p2, ~cp]
 
-        # mutate (change DNA based on normal distribution)
-        ks[:] = np.maximum(ks + (np.random.randn(*ks.shape)-0.5), 0.)    # must > 0
-        kv += ks * np.random.randn(*kv.shape)
-        kv[:] = np.clip(kv, *DNA_BOUND)    # clip the mutated value
-    return kids
+        self.pop = dict(DNA=5 * np.random.rand(1, self.DNA_size).repeat(self.pop_size, axis=0),   # initialize the pop DNA values
+                        mut_strength=np.random.rand(self.pop_size, self.DNA_size))                # initialize the pop mutation strength values
+
+    # find non-zero fitness for selection
+    def get_fitness(self, pred):     
+        v = (np.round(self.TARGET_DNA,1) == np.round(pred,1)).sum(axis=1)    
+        return v
 
 
-def kill_bad(pop, kids):
-    # put pop and kids together
-    for key in ['DNA', 'mut_strength']:
-        pop[key] = np.vstack((pop[key], kids[key]))
+    def make_kid(self):
+        # generate empty kid holder
+        kids = {'DNA': np.empty((self.n_kid, self.DNA_size))}
+        kids['mut_strength'] = np.empty_like(kids['DNA'])
 
-    fitness = get_fitness(pop['DNA'])            # calculate global fitness
-    idx = np.arange(pop['DNA'].shape[0])
-    good_idx = idx[fitness.argsort()][-POP_SIZE:]   # selected by fitness ranking (not value)
-    for key in ['DNA', 'mut_strength']:
-        pop[key] = pop[key][good_idx]
-    return pop
+        for kv, ks in zip(kids['DNA'], kids['mut_strength']):
+            # crossover (roughly half p1 and half p2)        
+            p1, p2 = np.random.choice(np.arange(self.pop_size), size=2, replace=False)
+            
+            cp = np.random.randint(0, 2, self.DNA_size, dtype=np.bool)  # crossover points                
+            kv[cp] = self.pop['DNA'][p1, cp]
+            kv[~cp] = self.pop['DNA'][p2, ~cp]
+            ks[cp] = self.pop['mut_strength'][p1, cp]
+            ks[~cp] = self.pop['mut_strength'][p2, ~cp]
+
+            # mutate (change DNA based on normal distribution)
+            ks[:] = np.maximum(ks + (np.random.randn(*ks.shape)-0.5), 0.)    # must > 0
+            kv += ks * np.random.randn(*kv.shape)
+            kv[:] = np.clip(kv, *self.DNA_bound)    # clip the mutated value
+        return kids
 
 
-pop = dict(DNA=5 * np.random.rand(1, DNA_SIZE).repeat(POP_SIZE, axis=0),   # initialize the pop DNA values
-           mut_strength=np.random.rand(POP_SIZE, DNA_SIZE))                # initialize the pop mutation strength values
+    def kill_bad(self, kids):
+        # put pop and kids together
+        for key in ['DNA', 'mut_strength']:
+            self.pop[key] = np.vstack((self.pop[key], kids[key]))
+
+        fitness = self.get_fitness(self.pop['DNA'])            # calculate global fitness
+        idx = np.arange(self.pop['DNA'].shape[0])
+        good_idx = idx[fitness.argsort()][-self.pop_size:]   # selected by fitness ranking (not value)
+        for key in ['DNA', 'mut_strength']:
+            self.pop[key] = self.pop[key][good_idx]
+        return self.pop
 
 
-print("DNA_SIZE:",DNA_SIZE)
-# for _ in range(N_GENERATIONS):
-i = 0 
-while (np.round(pop["DNA"][0],0) == np.round(TARGET_DNA,0)).sum() != DNA_SIZE:
-    # ES part    
-    kids = make_kid(pop, N_KID)
-    pop = kill_bad(pop, kids)   # keep some good parent for elitism
-    print("\r G:",i,np.round(pop["DNA"][0],0)," "*100, end="")
-    i+=1
-    
+    def evolve(self):
+        kids = self.make_kid()
+        self.kill_bad(kids)   # keep some good parent for elitism            
+        return np.round(self.pop["DNA"][0],0)
+
+
+if __name__ == '__main__':
+    ga = Learner(pop_size=400, n_kid=150)
+    i = 0 
+    for _ in range(N_GENERATIONS):
+        dna = ga.evolve()        
+        print("\r G:",i,dna," "*10, end="")
+        if (dna == np.round(TARGET_DNA,0)).sum() == len(dna): break
+        i+=1        
 print("\r\n")
---
+++ v7-feature-extractor.py
@@ -0,0 +1,113 @@
+import pandas as pd
+import time, datetime, os
+from jqdata import *
+
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+securities = get_index_stocks('000300.XSHG')
+start_date = datetime.date(2006,1,1)
+end_date = datetime.date(2016,12,31)
+
+res = pd.DataFrame()
+days = get_trade_days(start_date, end_date)
+
+def rolling_wr(val):
+    return (len(np.where(val>0)[0]) - len(np.where(val<0)[0])) / len(val)
+
+
+h_dataset = pd.DataFrame()
+h_data_filename = 'featured-v7.1-HS300-2006-2016.csv'
+if os.path.exists(h_data_filename):
+    print('Loading dataset')
+    h_dataset = pd.read_csv(h_data_filename,index_col=0)
+    print(h_dataset.shape)
+
+print('Days:' ,len(days))
+start_timestamp = time.time()
+for trade_date in days:
+    panel =  get_price(securities, end_date=trade_date, count=100, skip_paused=False)        
+    panel['pre_c3'] = round((panel['close'].shift(periods=3) - panel['close'].shift(periods=4)) / panel['close'].shift(periods=4)*100,2)
+    panel['pre_c2'] = round((panel['close'].shift(periods=2) - panel['close'].shift(periods=3)) / panel['close'].shift(periods=3)*100,2)
+    panel['pre_c1'] = round((panel['close'].shift(periods=1) - panel['close'].shift(periods=2)) / panel['close'].shift(periods=2)*100,2)
+    panel['today_c'] = round((panel['close'] - panel['close'].shift(periods=1)) / panel['close'].shift(periods=1)*100,2)
+
+    panel['up_l'] = round((panel['close'] - panel['high']) / panel['close']*100,2)
+    panel['dn_l'] = round((panel['close'] - panel['low']) / panel['close']*100,2)
+    panel['open_c'] = round((panel['open'] - panel['close'].shift(periods=1)) / panel['close'].shift(periods=1)*100,2)    
+    
+    panel['ma5'] = round(panel['close'].rolling(window=5).mean(),2)
+    panel['ma10'] = round(panel['close'].rolling(window=10).mean(),2)
+    panel['ma60'] = round(panel['close'].rolling(window=60).mean(),2)
+    
+    panel['ma5_pos']  = round((panel['close'] - panel['ma5'])/panel['close']*100,2)
+    panel['ma10_pos'] = round((panel['close'] - panel['ma10'])/panel['close']*100,2)    
+    panel['ma60_pos'] = round((panel['close'] - panel['ma60'])/panel['close']*100,2)    
+
+    
+    ma5_c = (panel['ma5'] - panel['ma5'].shift(periods=5)) / panel['ma5'].shift(periods=5)
+    panel['ma5_ang']  = round(np.arcsin(ma5_c/np.hypot(0.6, ma5_c/100))*180/math.pi*3,1)
+
+    ma10_c = (panel['ma10'] - panel['ma10'].shift(periods=5)) / panel['ma10'].shift(periods=5)
+    panel['ma10_ang']  = round(np.arcsin(ma10_c/np.hypot(0.6, ma10_c/100))*180/math.pi*3,1)
+
+    ma60_c = (panel['ma60'] - panel['ma60'].shift(periods=5)) / panel['ma60'].shift(periods=5)
+    panel['ma60_ang']  = round(np.arcsin(ma60_c/np.hypot(0.6, ma60_c/100))*180/math.pi*3,1)              
+
+
+    panel['p5_wr'] = round(panel['today_c'].rolling(window=5).sum(),2)
+    panel['p10_wr'] = round(panel['today_c'].rolling(window=10).sum(),2)    
+    
+    panel['prewr1'] = round(len(panel.major_xs(panel.major_axis[-2])[panel[:,panel.major_axis[-2]].today_c>=0])/\
+                         len(panel.major_xs(panel.major_axis[-2]).dropna(axis=0)),2)    
+    panel['win_r'] = round(len(panel.major_xs(panel.major_axis[-1])[panel[:,panel.major_axis[-1]].today_c>=0])/\
+                         len(panel.major_xs(panel.major_axis[-1]).dropna(axis=0)),2)
+
+    h100_high = panel['high'].rolling(window=100).max()
+    h100_low = panel['low'].rolling(window=100).min()
+    panel['h100_pos'] = round((panel['close'] - h100_low) / (h100_high - h100_low),2)    
+    
+    h10_high = panel['high'].rolling(window=10).max()
+    h10_low = panel['low'].rolling(window=10).min()
+    panel['h10_pos'] = round((panel['close'] - h10_low) / (h10_high - h10_low),2)
+    
+    panel = panel.drop(['open','close','high','low','ma5','ma10','ma60','volume','money'])
+    
+    res = panel.major_xs(panel.major_axis[-1]).dropna(axis=0)
+        
+    # attach future result for inspection
+    idx = list(days).index(trade_date)
+    future_len = 4
+    if idx+future_len >= len(days): break        
+    future_date = days[idx+future_len]
+    future_panel = get_price(securities, end_date=future_date, count=future_len+1, skip_paused=False)        
+    future_panel['fu_c1'] = round((future_panel['close'].shift(periods=-1) - future_panel['close']) / future_panel['close']*100,2)
+    future_panel['fu_c2'] = round((future_panel['close'].shift(periods=-2) - future_panel['close']) / future_panel['close']*100,2)
+    future_panel['fu_c3'] = round((future_panel['close'].shift(periods=-3) - future_panel['close']) / future_panel['close']*100,2)
+    future_panel['fu_c4'] = round((future_panel['close'].shift(periods=-4) - future_panel['close']) / future_panel['close']*100,2)    
+    
+    future_res = future_panel.major_xs(future_panel.major_axis[0]).dropna(axis=0)
+    future_res = future_res[['fu_c1','fu_c2','fu_c3','fu_c4']]        
+
+    res = res.join(future_res)
+    res['date'] = trade_date
+
+    cols = res.columns.tolist()
+    ncols = np.insert(cols[:-1],0, values=[cols[-1]])
+    res = res.reindex(columns=ncols)
+    
+    res.index.name = 'security'
+
+    remain_sec=int((time.time() - start_timestamp) / ((idx+1)/len(days)) * (1-((idx+1)/len(days))))
+    print('\r',trade_date, len(h_dataset),'records\tprogress: ',str(round((idx+1)/len(days)*100,2))+'%\t est: '+str(datetime.timedelta(seconds=remain_sec)),'\tsince start:',int((time.time() - start_timestamp))," "*50, end='')
+
+    
+    h_dataset = h_dataset.append(res)
+    if idx % 200 == 0 and idx>9:
+        h_dataset.to_csv(h_data_filename)
+        print('\r\nsave progress.\n')
+
+
+h_dataset.to_csv(h_data_filename)
+print('\ndone')

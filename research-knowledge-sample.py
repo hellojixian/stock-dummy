7e4be50032d8b32ecd3cdbf10cfac4e5921ddcd6
Jixian Wang <hellojixian@gmail.com>
2019-07-25 18:01:53 +0200

    update

--- research-knowledge-sample.py
+++ research-knowledge-sample.py
@@ -52,7 +52,7 @@ validation_set = pd.read_csv(data_source['validation'], index_col=0)
 def get_dist_report(knowledge, dataset, ranges):
     rs = dataset
     factors = dataset.columns.drop(['security','date','fu_c1','fu_c2', 'fu_c3', 'fu_c4']).values
-    df = eval(_compile_filter(factors, knowledge))
+    rs = eval(_compile_filter(factors, knowledge))
     dna = knowledge
 
     total_count = rs.shape[0]
@@ -111,34 +111,46 @@ if knowledge_id is None:
 else:
     k = kb.loc[knowledge_id]
 
-print(k.name)
-knowledge = k['knowledge']
-ranges = get_factor_ranges(knowledge, train_set, slice)
-train_report,train_wr,train_count = get_dist_report(knowledge, train_set, ranges)
-val_report,val_wr,val_count = get_dist_report(knowledge, validation_set, ranges)
-vmin,vmax=np.quantile(train_report.values,0.01),np.quantile(train_report.values,0.99)
 
-# 可视化 WR 的标准化
-fig =plt.figure(figsize=(16,6))
-ax1 =fig.add_subplot(121)
-ax2 =fig.add_subplot(122)
-sns.heatmap(train_report,
-            ax=ax1, cmap='RdYlGn_r',
-            linewidths=0.05, annot=False,
-            cbar=True, cbar_kws=dict(ticks=np.linspace(vmin,vmax,10)),
-            vmin=vmin, vmax=vmax)
-ax1.set_ylabel("features")
-ax1.set_xlabel("slices")
-ax1.set_title("Train   WR:{:.1f}%  Samples:{:.1f}K".format(train_wr*100,train_count/1000))
 
-sns.heatmap(val_report,
-            ax=ax2, cmap='RdYlGn_r',
-            linewidths=0.05, annot=False,
-            cbar=True, cbar_kws=dict(ticks=np.linspace(vmin,vmax,10)),
-            vmin=vmin, vmax=vmax)
-ax2.set_ylabel("features")
-ax2.set_xlabel("slices")
-ax2.set_title("Validation   WR:{:.1f}%  Samples:{:.1f}K".format(val_wr*100,val_count/1000))
-fig.suptitle("K: {} Slices: {:d}".format(k.name, slice))
-
+# plt.figure(figsize=(16,6))
+fig, (ax1,ax2) = plt.subplots(1, 2, figsize=(16,6))
+plt.ion()
 plt.show()
+
+for _ in range(10):
+    k = kb.sample(1).iloc[0]
+    knowledge = k['knowledge']
+    ranges = get_factor_ranges(knowledge, train_set, slice)
+    train_report,train_wr,train_count = get_dist_report(knowledge, train_set, ranges)
+    val_report,val_wr,val_count = get_dist_report(knowledge, validation_set, ranges)
+    vmin,vmax=np.quantile(train_report.values,0.01),np.quantile(train_report.values,0.99)
+
+    cbar_ax = fig.add_axes([0.92,0.2,0.01,0.6])
+    # 可视化 WR 的标准化
+    sns.heatmap(train_report,
+                ax=ax1, cmap='RdYlGn_r',
+                linewidths=0.05, annot=False,
+                cbar=True, cbar_kws=dict(ticks=np.linspace(vmin,vmax,10)),
+                cbar_ax=cbar_ax,
+                vmin=vmin, vmax=vmax)
+    ax1.set_ylabel("features")
+    ax1.set_xlabel("slices")
+    ax1.set_title("Train   WR:{:.1f}%  Samples:{:.1f}K".format(train_wr*100,train_count/1000))
+
+    sns.heatmap(val_report,
+                ax=ax2, cmap='RdYlGn_r',
+                linewidths=0.05, annot=False,
+                cbar=True, cbar_kws=dict(ticks=np.linspace(vmin,vmax,10)),
+                cbar_ax=cbar_ax,
+                vmin=vmin, vmax=vmax)
+    ax2.set_ylabel("features")
+    ax2.set_xlabel("slices")
+    ax2.set_title("Validation   WR:{:.1f}%  Samples:{:.1f}K".format(val_wr*100,val_count/1000))
+    fig.suptitle("K: {} Slices: {:d}".format(k.name, slice))
+    print('Drawing: ',k.name)
+    plt.draw()
+    plt.pause(2)
+
+print("Done")
+plt.ioff()

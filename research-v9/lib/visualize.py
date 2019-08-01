e68906c65e3d4a807d5630f6a11479fd8b315caf
Jixian Wang <hellojixian@gmail.com>
2019-08-02 00:38:20 +0200

    update visualize

--- research-v9/lib/visualize.py
+++ research-v9/lib/visualize.py
@@ -10,6 +10,8 @@ import datetime,time
 
 from pandas.plotting import register_matplotlib_converters
 
+DATE_FORMAT = "%Y-%m-%d"
+
 def generate_report(dataset):
     cols = ['short','median','long','close']
     cols.extend(['date'])
@@ -22,9 +24,11 @@ def calc_baseline_profit(baseline):
                     / baseline['close'].iloc[0] * 100,2)
 
 def visualize_report(dataset,backtest):
+    dataset = dataset.copy()
+    dataset.index = pd.to_datetime(dataset.index, format=DATE_FORMAT)
     baseline_profits = calc_baseline_profit(backtest)
 
-    xlabel = dataset.index.tolist()
+    xlabel = dataset.index.strftime(DATE_FORMAT).tolist()
     x = pd.to_datetime(dataset.index).tolist()
     register_matplotlib_converters()
 
@@ -43,6 +47,7 @@ def visualize_report(dataset,backtest):
     ax1.grid(color='gray',which='minor',linestyle='dashed',alpha=0.15)
     ax1.set_title("Baseline Profit: {:.2f}%".format(baseline_profits))
     ax1.set_ylabel('close price')
+    ax1.set_ylim(dataset['close'].min()*0.95,dataset['close'].max()*1.05)
 
     ax2 =plt.subplot(gs[2,:])
     ax2.plot(x, dataset['short'],label='short_pos', marker=".", alpha=0.4, color='r')
@@ -54,7 +59,7 @@ def visualize_report(dataset,backtest):
     ax2.set_xticks(x)
     ax2.xaxis.set_major_locator(mdates.DayLocator(interval=10))
     ax2.xaxis.set_minor_locator(mdates.DayLocator(interval=1))
-    ax2.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    ax2.xaxis.set_major_formatter(mdates.DateFormatter(DATE_FORMAT))
     ax2.set_xticklabels(xlabel,rotation=45, rotation_mode="default",alpha=0.5)
     ax2.set_yticks(np.round(np.linspace(0,7, 8),2))
     ax2.set_ylim(0,7)
@@ -68,26 +73,32 @@ def visualize_report(dataset,backtest):
     # ax2.axvspan(x[3], x[4], facecolor='g', alpha=0.15)
     # ax1.axvspan(x[3], x[4], facecolor='g', alpha=0.15)
     n_date = dataset.index[-30]
-    n_date = datetime.datetime.strptime(n_date, '%Y-%m-%d')
     init_pos = mdates.date2num(n_date)
 
     axvline1 = ax1.axvline(x=init_pos, color="k", linewidth=0.8, alpha=0.9)
     axvline2 = ax2.axvline(x=init_pos, color="k", linewidth=0.8, alpha=0.9)
 
     def onClick(event):
-        try:
-            pos = int(event.xdata)
-            axvline1.set_data([pos,pos], [0, 1])
-            axvline2.set_data([pos,pos], [0, 1])
-            plt.draw()
-        except:
-            return
+        if not event.xdata: return
+        pos = int(event.xdata)
+        date = mdates.num2date(pos).strftime(DATE_FORMAT)
+        if date not in dataset.index:
+            date = datetime.datetime.strptime(date, DATE_FORMAT)
+            ns = dataset[dataset.index<date]
+            n_date = dataset[dataset.index<date].iloc[-1].name
+            pos = mdates.date2num(n_date)
+
+        axvline1.set_data([pos,pos], [0, 1])
+        axvline2.set_data([pos,pos], [0, 1])
+        plt.draw()
 
     def onPress(event):
         global zoom_level
         data = axvline1.get_data()
         pos  = data[0][0]
-        date = mdates.num2date(pos).strftime('%Y-%m-%d')
+        date = mdates.num2date(pos).strftime(DATE_FORMAT)
+        if date not in dataset.index:
+            date = dataset[dataset.index<date].iloc[-1].name
         date_idx = dataset.index.get_loc(date)
 
         try:
@@ -96,31 +107,29 @@ def visualize_report(dataset,backtest):
             zoom_level = None
         if event.key=='A' or event.key=='a' or event.key=='left':
             n_date = dataset[date_idx-1:date_idx].index[0]
-            n_date = datetime.datetime.strptime(n_date, '%Y-%m-%d')
             pos = mdates.date2num(n_date)
         elif event.key=='D' or event.key=='d' or event.key=='right':
             n_date = dataset[date_idx+1:date_idx+2].index[0]
-            n_date = datetime.datetime.strptime(n_date, '%Y-%m-%d')
             pos = mdates.date2num(n_date)
         elif event.key=='Q':
             zoom_level = 30
         elif event.key=='W' or event.key=='w' or event.key=='up':
             zoom_level = 20
-        elif event.key=='E' or event.key=='e' or event.key=='down':
+        elif event.key=='E' or event.key=='e':
             zoom_level = 10
-        elif event.key=='S' or event.key=='escape':
+        elif event.key=='S' or event.key=='escape' or event.key=='down':
             zoom_level = None
 
         axvline1.set_data([pos,pos], [0, 1])
         axvline2.set_data([pos,pos], [0, 1])
 
         if zoom_level is None:
-            ax1.set_ylim(dataset['close'].min(),dataset['close'].max())
+            ax1.set_ylim(dataset['close'].min()*0.95,dataset['close'].max()*1.05)
             ax1.set_xlim(x[0],x[-1])
             ax2.set_xlim(x[0],x[-1])
         else:
             slice = dataset[date_idx-zoom_level:date_idx+zoom_level]
-            ax1.set_ylim(slice['close'].min(),slice['close'].max())
+            ax1.set_ylim(slice['close'].min()*0.95,slice['close'].max()*1.05)
             ax1.set_xlim(pos-zoom_level,pos+zoom_level)
             ax2.set_xlim(pos-zoom_level,pos+zoom_level)
         plt.draw()

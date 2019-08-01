d54f4fdc2bf3a5c1632eb5353e6736dbe5054257
Jixian Wang <hellojixian@gmail.com>
2019-08-01 18:51:53 +0200

    update

--- research-v9/lib/feature_extract.py
+++ research-v9/lib/feature_extract.py
@@ -12,20 +12,23 @@ def to_categorial(v, categories):
     val = v/(1/(categories-1))
     return int(np.round(val+1))
 
-def extract_features(security,trade_date,get_price):
-    close = get_price(security=security, end_date=trade_date, count=1)['close'][0]
-    days_scopes=[3,5,10,20,30,60,120]
+def extract_features(security,trade_date,get_price,close=None):
+    days_scopes=[120,60,30,20,10,5,3]
     n_steps = 7
+
+    history = get_price(security=security, end_date=trade_date, count=days_scopes[0]+1)
+    if close is None:
+        close = history.iloc[-1]['close']
+    history = history.iloc[:-1]
 
     feature = {}
     for days in days_scopes:
-        history = get_price(security=security, end_date=trade_date, count=days)
-        history = history.iloc[:-1]
+        history = history[-days:]
         min, max = history['low'].min(), history['high'].max()
         pos = to_categorial((close-min) / (max-min), n_steps)
         f_key = 'pos_{}'.format(days)
         feature[f_key]= pos
-
+    
     feature['short']  = np.round(np.mean([feature['pos_3'],feature['pos_5'],feature['pos_10']]),1)
     feature['median'] = np.round(np.mean([feature['pos_20'],feature['pos_30']]),1)
     feature['long']   = np.round(np.mean([feature['pos_60'],feature['pos_120']]),1)

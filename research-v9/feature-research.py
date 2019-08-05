fbe6685e3d13c91d1f46f43df4113042b22737a8
Jixian Wang <hellojixian@gmail.com>
2019-08-05 14:50:32 +0200

    update

--- research-v9/feature-research.py
+++ research-v9/feature-research.py
@@ -23,8 +23,8 @@ pd.set_option('display.width', 1000)
 
 security='600822.XSHG'
 # security='600001.XSHG'
-start_date=datetime.date(2004,7,30)
-end_date=datetime.date(2010,12,30)
+start_date=datetime.date(2006,7,30)
+end_date=datetime.date(2011,12,30)
 
 train_df = get_train_set(300, start_date, end_date)
 backtest = get_price(security=security, start_date=start_date, end_date=end_date)

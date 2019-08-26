880950b1af1dbc87fb10eb8355f2d75548c0a4d1
Jixian Wang <hellojixian@gmail.com>
2019-08-26 11:56:57 +0200

    update

--- research-v10/lib/dna.py
+++ research-v10/lib/dna.py
@@ -2,7 +2,7 @@ def gen_DNAset(len):
     bits=[]
     max = "0b"+(''.join(str(e) for e in ['1']*len))
     for i in range(int(max,2)):
-        s =  ("{0:0"+str(DNA_LEN)+"b}").format(i)
+        s =  ("{0:0"+str(len)+"b}").format(i)
         bits.append(s)
     return bits
 

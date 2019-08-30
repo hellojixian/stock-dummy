f63b5934527a947e358831939f8385c23bc88509
Jixian Wang <hellojixian@gmail.com>
2019-08-30 15:33:13 +0200

    update

--- research-v10/verify-kb.py
+++ research-v10/verify-kb.py
@@ -74,8 +74,8 @@ for trading_date in trading_dates:
     score_mean=rs['score'].mean()
     score_q95=rs['score'].quantile(0.95)
     rs = rs[(rs.score>=10)]
-    rs = rs.sort_values(by=['prev_2','today'],ascending=False)
-    rs = rs.sample(6,replace=True)
+    rs = rs.sort_values(by=['score'],ascending=False)
+    if rs.shape[0]>=10: rs = rs[:10].sample(6)
     rs['score'] = np.round(rs['score'],3)
     rs = rs[['date','security','close','prev_2','today','score','fu_1','fu_2','fu_3','fu_3','fu_4']]
     print("\n")

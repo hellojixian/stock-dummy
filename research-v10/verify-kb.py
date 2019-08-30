b87e4d1d6985c53ba1e532eee1a4831706d45388
Jixian Wang <hellojixian@gmail.com>
2019-08-30 13:56:53 +0200

    update

--- research-v10/verify-kb.py
+++ research-v10/verify-kb.py
@@ -40,7 +40,7 @@ for trading_date in trading_dates:
             if k.shape[0]==0:
                 score += 0.5
             else:
-                score += (k.iloc[0]['wr_f2'])
+                score += k.iloc[0]['wr_f1']+k.iloc[0]['wr_f2']
 
         l.acquire()
         finished.value+=1
@@ -70,7 +70,7 @@ for trading_date in trading_dates:
     # rs = rs.sort_values(by=['score'],ascending=False)
     score_mean=rs['score'].mean()
     score_q95=rs['score'].quantile(0.95)
-    rs = rs[(rs.score>=5)]
+    rs = rs[(rs.score>=8)]
     rs = rs.sort_values(by=['prev_2','today'],ascending=True)
     rs = rs[:5]
     rs['score'] = np.round(rs['score'],3)

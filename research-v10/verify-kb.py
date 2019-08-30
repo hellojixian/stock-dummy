bb88304ce9cec7aa657639d780a82870fa325273
Jixian Wang <hellojixian@gmail.com>
2019-08-30 16:14:46 +0200

    update

--- research-v10/verify-kb.py
+++ research-v10/verify-kb.py
@@ -81,9 +81,9 @@ for trading_date in trading_dates:
     rs = rs.sort_values(by=['score'],ascending=False)
     rs = rs[:10]
     if rs['prev_2'].mean()>0:
-        rs = rs.sort_values(by=['prev_2'],ascending=True)
-    else:
         rs = rs.sort_values(by=['prev_2'],ascending=False)
+    else:
+        rs = rs.sort_values(by=['prev_2'],ascending=True)
     rs = rs[:4]
 
     rs['score'] = np.round(rs['score'],3)

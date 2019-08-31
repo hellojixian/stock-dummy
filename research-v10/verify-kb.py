9c179fcfbd32be47ffd55ec7cfc6828544e12d24
Jixian Wang <hellojixian@gmail.com>
2019-08-31 22:54:44 +0200

    update

--- research-v10/verify-kb.py
+++ research-v10/verify-kb.py
@@ -23,9 +23,9 @@ print('Trading dates loaded')
 
 
 total_profit = 1
-cw,cwp = 0,0
-cl,clp = 0,0
-prev_rr,prev_rr_2 = 0,0
+
+prev_close_ma,prev_close_ma2,prev_close_ma3 = 0,0,0
+skip_days = 0
 dataset['prev_changes'] = dataset['prev_0']+dataset['prev_1']+dataset['prev_2']+dataset['prev_3']+dataset['prev_4']
 
 for trading_date in trading_dates:
@@ -35,54 +35,67 @@ for trading_date in trading_dates:
 
     subset = dataset[dataset.index==trading_date]
     total = subset.shape[0]
+
+    subset = subset.sort_values(by=['close'],ascending=True)
+    close_ma = subset[100:400]['close'].mean()
     wr = subset[subset.prev_0>0].shape[0] / total
     today_rr = subset[subset.pos_60<=20].shape[0] / total
+    today_rrh = subset[subset.pos_20>=80].shape[0] / total
 
-    query = "(prev_0<9.5) & (prev_0>-9.5) & prev_changes>=-20 & trend_10==0"
+    query = "trend_10==0 &  (prev_0<=9 & prev_0>=-4)"
     subset = subset[subset.eval(query)]
 
     rs = subset
+    rs = rs.sort_values(by=['money'],ascending=True)
+    rs = rs[:500]
+    rs = rs.sort_values(by=['close'],ascending=True)
+    rs = rs[:200]
     rs = rs.sort_values(by=['prev_changes'],ascending=True)
+    rs = rs[:100]
+    rs = rs.sort_values(by=['pos_vol_10'],ascending=True)
     rs = rs[:20]
     rs = rs.sort_values(by=['pos_90'],ascending=True)
     rs = rs[:10]
 
-    rs = rs[['security','close','prev_changes','prev_0','fu_1']]
+    rs = rs[['security','close','prev_changes','prev_1','prev_0','fu_1']]
 
-    if rs.shape[0]>2 :
+    if rs.shape[0]>4 :
         print("="*120)
         print(rs)
         print("="*120)
 
-
+        ma_p0 = (close_ma+prev_close_ma)/2
+        ma_p1 = (prev_close_ma+prev_close_ma2)/2
+        ma_diff = ma_p0-ma_p1
         profit = rs['fu_1'].mean()
-        if ((prev_rr>today_rr or prev_rr_2 > today_rr) or today_rr<0.1): # clp>-2.5 or cl>4 or clp<-8.5:
-            profit = rs['fu_1'].mean()
-            total_profit = total_profit*(1+(profit/100))
+        # if ((prev_rr>today_rr or prev_rr_2 > today_rr) or today_rr<0.1): # 最高收益185倍
+        if  close_ma-prev_close_ma>=0:
+
+            if skip_days>0:
+                skip_days-=1
+                print(">>"*20,"skipped","<<"*20)
+            else:
+                profit = rs['fu_1'].mean()
+                total_profit = total_profit*(1+(profit/100))
         else:
-            print("skipped")
+            print(">>"*20,"skipped","<<"*20)
 
-        print("{:06}\tDate: {}\t Profit: {:.2f}%\t Total: {:.2f}%\t\t wr: {:.3f}\t rr: {:.3f}\tcw/l: {:.2f}\t{:.2f}".format(
-                    date_i,trading_date,profit,total_profit*100,wr, today_rr, cl, clp))
 
-        prev_rr = today_rr
-        prev_rr_2 =prev_rr
+        sign = "+" if ma_diff>0 else ""
         profit = rs['fu_1'].mean()
-        if profit>=0:
-            cwp+=profit
-            cl=0
-            clp=0
-        else:
-            cw=0
-            cwp=0
-            cl+=1
-            clp +=profit
+        print("{:06}\t{}\t Profit: {:.2f}%\t Total: {:.2f}%\t wr: {:.3f}\t rr: {:.3f}\tclose_ma: {:.2f} ({}{:.2f})\t skip:{}".format(
+                    date_i,trading_date,profit,total_profit*100,wr, today_rr, close_ma, sign,ma_diff,skip_days))
+
+        prev_close_ma = close_ma
+        prev_close_ma2 = prev_close_ma
+        prev_close_ma3 = prev_close_ma2
+
 
         if (profit<-5):
             print(">"*100)
             print("<"*100)
             print("\n\n")
     else:
-        print("{:06}\tDate: {}\t rr: {:.3f} - Ignored".format(date_i,trading_date,today_rr))
-
+        if skip_days==0: skip_days = 3
+        print(">>>>>>BIG SKIP<<<<<<")
     print("\n")

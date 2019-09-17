d992b84f7393a71b3ea3250975b6f14bf0b22234
Jixian Wang <hellojixian@gmail.com>
2019-09-17 16:17:48 +0200

    update

--- research-v11/r2.py
+++ research-v11/r2.py
@@ -45,6 +45,7 @@ fund = init_fund
 ideal_buy_price = 0
 within_days=0
 recent_changes,skip_until_price_lower_than = 0,0
+drop_rate = 0
 for i in range(1330, len(history)):
 # for i in range(60, len(history)):
     should_buy_close = False
@@ -56,19 +57,9 @@ for i in range(1330, len(history)):
     high    = subset['high'].iloc[-1]
     last_change = subset['change'].iloc[-1]*100
 
-    ideal_buy_price = np.min(subset['close'][:-3].values)
-    bar_body = abs(open-close)/subset['close'].iloc[-2]
-    upline = abs(high - max([open,close]))/subset['close'].iloc[-2]
+    prev_change = subset['change'].iloc[-2]*100
+    prev_close = subset['close'].iloc[-2]
 
-    trend = 'down'
-    v_min, v_max = np.min(subset['close'][-10:]), np.max(subset['close'][-10:])
-    v_min_id, v_max_id = subset['close'][-10:][subset.close==v_min].index[-1],subset['close'][-10:][subset.close==v_max].index[-1]
-    if v_max_id<=v_min_id: trend ='up'
-
-    # if skip_until_price_lower_than <= close and skip_until_price_lower_than>0:
-    #     skip_days=10
-    # else:
-    #     skip_until_price_lower_than = 0
 
 
     if position == 'empty':
@@ -78,30 +69,23 @@ for i in range(1330, len(history)):
             should_buy_close = True
             should_max_hold_days = 10
 
-        # if subset['change'].iloc[-2]<=-0.07 \
-        #     and subset['change'].iloc[-1]>=0.01:
-        #     should_buy_close = True
-        #     should_max_hold_days = 10
+        if last_change>0 and prev_change<0:
+            lowest_since_days = 1
+            for i in range(2,60):
+                if subset['close'][(-i):-1].min() < prev_close:
+                    # highest = subset['close'][(-i):].max()
+                    # drop_rate = (highest - subset['close'][(-i):].min()) / highest
+                    break
+                lowest_since_days+=1
+            print("{} {:.4f}".format(lowest_since_days, drop_rate))
 
-        #一绿吃掉5日涨幅则买入 并且属于上升波段
-        # if subset['close'][-5:-1].min() > subset['close'].iloc[-1].min() \
-        #     and last_change <-0.02 and trend=='up':
-        #     should_buy_close = True
-        #     should_max_hold_days = 10
+            if lowest_since_days>=5:
+                should_buy_close = True
+                should_max_hold_days = 10
 
     if position == 'full':
         profit = (close - bought_price)/bought_price*100
-        if profit>=10 or hold_days>=should_max_hold_days or last_change<-1:
-            should_sell=True
-
-        #如果上影线大于实体
-        if upline>bar_body and bar_body>=0.005 :
-            should_sell=True
-
-        if subset['change'].iloc[-1]<0 \
-            and subset['change'].iloc[-2]>0 \
-            and subset['change'].iloc[-3]<0 \
-            and subset['change'].iloc[-4]<0:
+        if profit>=10 or hold_days>=should_max_hold_days or last_change<0.:
             should_sell=True
 
         if last_change<-9:
@@ -142,11 +126,6 @@ for i in range(1330, len(history)):
                 action="sold"
                 total_session+=1
                 if profit>0: win_session+=1
-                # v_min2, v_max2 = np.min(subset['close'][-60:]), np.max(subset['close'][-60:])
-                # if close == v_max2:
-                #     recent_changes = (v_max2 - v_min2) / v_min2
-                #     if recent_changes>0.15 :
-                #         skip_until_price_lower_than=close* (1-recent_changes*0.5)
 
 
     profit = ((fund + bought_amount*close - init_fund) / init_fund) * 100

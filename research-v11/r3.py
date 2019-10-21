6e4eca109ce0b89a52cf0d1232120c22497fc05a
Jixian Wang <hellojixian@gmail.com>
2019-10-21 17:36:31 +0200

    update

--- research-v11/r3.py
+++ research-v11/r3.py
@@ -5,8 +5,8 @@ import numpy as np
 import math, sys, os
 
 # SECURITY = 'sz002627'
-SECURITY = 'sz000001'
-# SECURITY = 'sz002620'
+# SECURITY = 'sz000001'
+SECURITY = 'sz002620'
 np.random.seed(0)
 
 # set output
@@ -32,21 +32,21 @@ history['open_jump'] = np.round((history['open']-history['close'].shift(periods=
 history['amp'] = np.round((history['high']-history['low']) / history['close'].shift(periods=1)*100,2)
 history['upline'] = np.round((history['high']-history['open']) / history['close'].shift(periods=1)*100,2)
 history['downline'] = np.round((history['open']-history['low']) / history['close'].shift(periods=1)*100,2)
-for i in [3,5,7]:
+for i in [2,3,5,7]:
     history['turnover_ma{}'.format(i)] = np.round(history['turnover'].rolling(window=i).mean(),2)
     history['changes_{}d'.format(i)] = np.round((history['close']-history['close'].shift(periods=i)) / history['close'].shift(periods=i)*100,2)
 
 
 slices=9
 for i in range(1,slices):
-    print("{:3d}  amp:{}\tbar:{:5.2f}\tchg:{:5.2f}\topn:{:5.2f}\ttov:{:5.2f}\ttov3:{:5.2f}\ttov5:{:5.2f}\tc3d:{:5.2f}\tc5d:{:5.2f}\tc7d:{:5.2f}\t ".format(i,
+    print("{:3d}  amp:{}\tbar:{:5.2f}\ttov:{:5.2f}\ttov2:{:5.2f}\ttov3:{:5.2f}\ttov5:{:5.2f}\tc2d:{:5.2f}\tc3d:{:5.2f}\tc5d:{:5.2f}\tc7d:{:5.2f}\t ".format(i,
         np.round(history['amp'].quantile(i/slices),2),
         np.round(history['bar'].quantile(i/slices),2),
-        np.round(history['change'].quantile(i/slices),2),
-        np.round(history['open_jump'].quantile(i/slices),2),
         np.round(history['turnover'].quantile(i/slices),2),
+        np.round(history['turnover_ma2'].quantile(i/slices),2),
         np.round(history['turnover_ma3'].quantile(i/slices),2),
         np.round(history['turnover_ma5'].quantile(i/slices),2),
+        np.round(history['changes_2d'].quantile(i/slices),2),
         np.round(history['changes_3d'].quantile(i/slices),2),
         np.round(history['changes_5d'].quantile(i/slices),2),
         np.round(history['changes_7d'].quantile(i/slices),2),
---
+++ research-v11/r5.py
@@ -0,0 +1,51 @@
+'''
+每种策略应该都对应自己的风险控制，由于买入原则不同 风险控制机制也应该不同
+可以单独被训练
+
+
+长策略
+    下跌一个波段，前压力位
+    变量：
+    - 前压力位置
+
+W底部匹配
+    下跌后 反弹 再次回踩则买入
+
+短策略
+    最多持有3天
+    - 瞬间超跌反弹
+
+追涨策略
+    红柱之后3日高位十字星
+    吃到红柱就出来
+
+
+'''
+
+class strategy(object):
+    def __init__(self,dataset):
+        self.stop_winning = NULL
+        self.stop_lossing = NULL
+        return
+
+    def should_buy(dataset):
+        decision = False
+        return
+    def should_sell(self, dataset):
+        return
+
+    def fitting(dataset):
+        
+        pass
+
+    def _update(self, dataset):
+        return
+
+pattern_triggers = []
+def should_buy(subset):
+    decision = False
+    for func in pattern_triggers:
+        if func(subset):
+            decision = True
+            break
+    return decision
